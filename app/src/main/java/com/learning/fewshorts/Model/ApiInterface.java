68fc51d54ab833f1497b448a1018b9614fd62055
Versatile vasu <Versatile.vasu@outlook.com>
2020-03-13 23:14:35 +0530

    Commit Retrofit ApiClient and Models added although wrong package Anyway

---
+++ app/src/main/java/com/learning/fewshorts/Model/ApiInterface.java
@@ -0,0 +1,18 @@
+package com.learning.fewshorts.Model;
+
+import com.learning.fewshorts.RecievedData.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+        @Query("country") String country,
+        @Query("category") String category,
+        @Query("apiKey") String apiKey
+    );
+
+}
